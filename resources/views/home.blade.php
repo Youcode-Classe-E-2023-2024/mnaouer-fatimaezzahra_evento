d464572402810156091f183df422bae38fddd252
fatima ezzahra mnaouer <fatimaezzahra.mnaouer@gmail.com>
2024-03-10 19:59:20 +0000

    patch home ui

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,26 +4,27 @@
 @endpush
 
 @section('content')
-<main class="container pt-4">
-    <div class="row mb-2" id="content">
-        <div class="col-md-6">
-            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+    <main class="container pt-4">
+        <div class="row mb-2" id="content">
                 @foreach($events as $event)
-                <div class="col p-4 d-flex flex-column position-static">
-                    <strong class="d-inline-block mb-2 text-primary">category</strong>
-                    <h3 class="mb-0">{{ $event->title }}</h3>
-                    <div class="mb-1 text-muted">{{ $event->started_at }}</div>
-                    <p class="card-text mb-auto text-muted">{{ ucfirst($event->location) }}</p>
-                    <a href="{{ route('event.show', $event->id) }}" class="stretched-link">Details</a>
-                </div>
-                <div class="col-auto d-none d-lg-block">
-                    <img src="{{ $event->picture }}" alt="cover" class="bd-placeholder-img" width="300" height="250">
-                </div>
+                    <div class="col-md-6">
+                        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                            <div class="col p-4 d-flex flex-column position-static">
+                                <strong class="d-inline-block mb-2 text-primary">category</strong>
+                                <h3 class="mb-0">{{ $event->title }}</h3>
+                                <div class="mb-1 text-muted">{{ $event->started_at }}</div>
+                                <p class="card-text mb-auto text-muted">{{ ucfirst($event->location) }}</p>
+                                <a href="{{ route('event.show', $event->id) }}" class="stretched-link">Details</a>
+                            </div>
+                            <div class="col-auto d-none d-lg-block">
+                                <img src="{{ $event->picture }}" alt="cover" class="bd-placeholder-img" width="300"
+                                     height="250">
+                            </div>
+                        </div>
+                    </div>
                 @endforeach
-            </div>
         </div>
-    </div>
-</main>
+    </main>
 @endsection
 
 @push('js')
