5b4d16b966772d7679ce83c65eff4d52075982dc
fatima ezzahra mnaouer <fatimaezzahra.mnaouer@gmail.com>
2024-03-11 20:46:52 +0000

    add count dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Event;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+
     public function index() {
-        return view('dashboard');
+
+        $usersCount = User::count();
+        $categoriesCount = Category::count();
+        $StatusAccepted= Event::where('status','Accepted')->count();
+        $StatusRejected= Event::where('status','Rejected')->count();
+
+        return view('dashboard', compact('usersCount','categoriesCount','StatusAccepted','StatusRejected'));
     }
+
 }
