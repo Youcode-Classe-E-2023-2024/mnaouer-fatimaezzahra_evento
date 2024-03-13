486b366dd7439ce7a48a499b7a1853a5dbad528f
fatima ezzahra mnaouer <fatimaezzahra.mnaouer@gmail.com>
2024-03-13 02:59:46 +0000

    secure routes

--- routes/web.php
+++ routes/web.php
@@ -21,51 +21,53 @@ use App\Http\Controllers\DashboardController;
 |
 */
 
-Route::middleware(['role:admin'])->group(function () {
+Route::middleware(['guest'])->group(function () {
+    Route::get('/register', [RegisterController::class, 'create'])->name('register');
+    Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
 
-});
+    Route::get('/login', [LoginController::class, 'create'])->name('login');
+    Route::post('/login', [LoginController::class, 'store'])->name('login.store');
 
-Route::middleware(['role:admin|organizer'])->group(function () {
+    Route::get('/forgot-password', [ForgotPasswordController::class, 'create'])->name('password.create');
+    Route::post('/forgot-password', [ForgotPasswordController::class, 'store'])->name('password.store');
+    Route::get('/forgot-password/{token}', [ForgotPasswordController::class, 'edit'])->name('password.reset');
+    Route::post('/forgot-password/reset', [ForgotPasswordController::class, 'update'])->name('password.update');
 
 });
 
 Route::middleware(['role:admin|organizer|user'])->group(function () {
+    Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
 
+    Route::get('/profile/show/{article}', [ProfileController::class, 'show'])->name('profile.show');
+
+});
+
+Route::middleware(['role:admin|organizer'])->group(function () {
+    Route::get('/event/create', [EventController::class, 'create'])->name('event.create');
+    Route::get('/event', [EventController::class, 'index'])->name('event');
+    Route::post ('/event/store', [EventController::class, 'store'])->name('event.store');
+    Route::get('/event/edit/{article}', [EventController::class, 'edit'])->name('event.edit');
+    Route::post('/event/update', [EventController::class, 'update'])->name('event.update');
+});
+
+Route::middleware(['role:admin'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+    Route::post('/profile/role', [ProfileController::class, 'editRole'])->name('profile.role');
+
+    Route::get('/category', [CategoryController::class, 'index'])->name('category');
+    Route::post('/category/store', [CategoryController::class, 'store'])->name('category.store');
+    Route::post('/category/update', [CategoryController::class, 'update'])->name('category.update');
+    Route::post('/category/destroy', [CategoryController::class, 'destroy'])->name('category.destroy');
+
+    Route::post('/event/status', [EventController::class, 'changeStatus'])->name('event.status');
+    Route::post('/event/destroy', [EventController::class, 'destroy'])->name('event.destroy');
 });
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/category/{keyWord}', [EventController::class, 'byCatName'])->name('category.search');
+Route::post('/title', [EventController::class, 'byTitle'])->name('title.search');
 
-Route::get('/register', [RegisterController::class, 'create'])->name('register');
-Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
-
-Route::get('/login', [LoginController::class, 'create'])->name('login');
-Route::post('/login', [LoginController::class, 'store'])->name('login.store');
-
-Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
-
-Route::get('/forgot-password', [ForgotPasswordController::class, 'create'])->name('password.create');
-Route::post('/forgot-password', [ForgotPasswordController::class, 'store'])->name('password.store');
-Route::get('/forgot-password/{token}', [ForgotPasswordController::class, 'edit'])->name('password.reset');
-Route::post('/forgot-password/reset', [ForgotPasswordController::class, 'update'])->name('password.update');
-
-
-Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
-Route::post('/profile/role', [ProfileController::class, 'editRole'])->name('profile.role');
-Route::get('/profile/show/{article}', [ProfileController::class, 'show'])->name('profile.show');
-
-Route::get('/event', [EventController::class, 'index'])->name('event');
-Route::get('/event/create', [EventController::class, 'create'])->name('event.create');
-Route::post ('/event/store', [EventController::class, 'store'])->name('event.store');
-Route::get('/event/edit/{article}', [EventController::class, 'edit'])->name('event.edit');
-Route::post('/event/update', [EventController::class, 'update'])->name('event.update');
 Route::get('/event/{article}', [EventController::class, 'show'])->name('event.show');
-Route::post('/event/status', [EventController::class, 'changeStatus'])->name('event.status');
-Route::post('/event/destroy', [EventController::class, 'destroy'])->name('event.destroy');
-
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
-Route::get('/category', [CategoryController::class, 'index'])->name('category');
-Route::post('/category/store', [CategoryController::class, 'store'])->name('category.store');
-Route::post('/category/update', [CategoryController::class, 'update'])->name('category.update');
-Route::post('/category/destroy', [CategoryController::class, 'destroy'])->name('category.destroy');
